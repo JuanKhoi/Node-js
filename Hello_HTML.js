718376ded0a343a86883b37ad0e83177436283ee
JuanKhoi <juancasor.hlpe@gmail.com>
2016-07-31 10:48:46 -0500

    Add Header

--- Hello_HTML.js
+++ Hello_HTML.js
@@ -5,6 +5,7 @@ var http = require("http"),
 
 http.createServer(function(req,res){
 	fs.readFile("./index.html", function(err,html){
+	res.writeHead(200,{"Content-Type":"text/html"})
 	res.write(html);
 	res.end();
 	});
